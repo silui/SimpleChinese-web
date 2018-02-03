f3da6370a63979063c41f2a5e5498ff654fa818b
silui <silui@ucsc.edu>
2018-02-03 10:23:16 -0800

    vocabselector module added

--- vocabset/vocab.js
+++ vocabset/vocab.js
@@ -1,20 +1,19 @@
 // console.log('something');
 // document.getElementById('vocabField').innerHTML='not hello';
 firebaseInit();
-// var finaljson={'name':'edward','date':'12'};
 var dbRef=firebase.database().ref("vocab");
 dbRef.on("value",function(snapshot)
 {
-  var tempjson=snapshot.val();
-  finaljson=Object.keys(tempjson);
-  console.log(Object.keys(tempjson));
-  document.getElementById('vocabField').innerHTML=`
-  ${finaljson.map(function(levelset){
+  var vocabListAll=snapshot.val();
+  var leftside=Object.keys(vocabListAll);
+  var rightside=Object.values(vocabListAll);
+  console.log(rightside[0].url);
+  document.getElementById('vocabField').innerHTML=
+  `<div class='char-book'>
+
+  ${leftside.map(function(levelset,index){
     return `
-    <div class='char-book'>
-    ${levelset}
-    </div>
+    <img src='${rightside[index].url}'>
     `
   }).join('')}
-  `
-});
+  </div>`;});
