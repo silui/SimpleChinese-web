cd3fad6b4f9c99842c9fbbfbc09965ffb05be1ef
silui <silui@ucsc.edu>
2018-02-04 17:31:22 -0800

    merged sound

--- study/study.js
+++ study/study.js
@@ -1,12 +1,11 @@
 firebaseInit();
-
 var dbRef=firebase.database().ref("vocab");
 dbRef.on("value",function(snapshot)
 {
   var vocabListAll = snapshot.val();
   var levels = Object.keys(vocabListAll);
   var curStudy = Object.values(vocabListAll);
-  
+
 
 
   var i = 0; //No. of char in the list
@@ -15,12 +14,12 @@ dbRef.on("value",function(snapshot)
   var vocabSet = splitUrl[1]-1;
   var vocabSetLength = Object.values(curStudy[vocabSet]).length;
 
-    console.log(vocabSetLength);
+  //  console.log(vocabSetLength);
 
   function showChar(){
   	document.getElementById('studyField').innerHTML =
 	`<div>${curStudy[vocabSet][i].pinyin}</div>
-	<h2>${curStudy[vocabSet][i].hanzi}</h2>`;
+	<h2 id"character">${curStudy[vocabSet][i].hanzi}</h2>`;
 
 	document.getElementById('transField').innerHTML = showTrans();
  }
@@ -37,7 +36,6 @@ dbRef.on("value",function(snapshot)
  		return temp;
  	}
 
-
   function clickForPre(){
   	if(i > 0)
   	{
@@ -51,13 +49,22 @@ dbRef.on("value",function(snapshot)
   	{
   		i++;
   		showChar();
-  	}	
+  	}
+  }
+
+  function clickForSound(){
+    var targetChar=curStudy[vocabSet][i].hanzi;
+    targetChar=encodeURI(targetChar);
+    const soundUrl = `http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&spd=2&text=${targetChar}`;
+    document.getElementById('src').setAttribute('src', soundUrl);
+    const aud = document.getElementById('aud');
+    aud.load();
+    return aud.play();
   }
 
   //console.log(curStudy[tempValueSetLater][i].hanzi);
   document.getElementById("clickP").onclick = function(){clickForPre()};
   document.getElementById("clickN").onclick = function(){clickForNext()};
-
+  document.getElementById("soundButton").onclick = function(){clickForSound()};
   showChar();
-
 });
