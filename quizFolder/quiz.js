4d8058a2003fb54512e1d846f4f48cc03a46d888
silui <silui@ucsc.edu>
2018-03-01 22:07:20 -0800

    half of quiz implemented, still needs to link with firebase for correctness and bug fix

--- quizFolder/quiz.js
+++ quizFolder/quiz.js
@@ -8,20 +8,50 @@ dbRef.on("value",function(snapshot)
   var vocabListAll = snapshot.val();
   var curStudy = Object.values(vocabListAll);
   var levelArray = curStudy[targetLevel];
-  var tempArray = Object.values(levelArray)
-  var wholeArray = tempArray.slice(targetSetStart,targetSetStart+25);
-  var answerBoxes = document.getElementsByClassName('answer');
-  var translationArray = getFourTranslation("good",tempArray);
-  assignAnswer(answerBoxes,translationArray)
+  tempArray = Object.values(levelArray)
+  globalWholeArray= tempArray.slice(targetSetStart,targetSetStart+25);
+  var buttonArray = document.getElementsByClassName('answer');
+  for(var i=0; i<buttonArray.length; i++)
+  {
+    buttonArray[i].addEventListener("click",function()
+    {
+      next();
+    });
+  }
+  next();
+  // var answerBoxes = document.getElementsByClassName('answer');
+  // var translationArray = getFourTranslation("good",tempArray);
+  // assignAnswer(answerBoxes,translationArray)
 });
+var tempArray = [];
+var globalWholeArray=[];
+var iter=-1;
+function next()
+{
+  if(iter===24){
+    window.location.href='./'
+  }
+  else {
+    iter++;
+    // console.log(globalWholeArray[iter]);
+    setChineseChar(globalWholeArray[iter].hanzi);
+    var correctAnswer = globalWholeArray[iter].translations;
+    // var answerBoxes = document.getElementsByClassName('answer');
+    var translationArray = getFourTranslation(correctAnswer,tempArray);
+    assignAnswer(document.getElementsByClassName('answer'),translationArray)
+  }
+}
 
+function setChineseChar(inputChar)
+{
+  document.getElementById('charField').innerHTML=inputChar;
+}
 function assignAnswer(buttonArray, answerArray)
 {
-  console.log(buttonArray);
-  // buttonArray.forEach(function(elem)
-  // {
-  //   elem.innerText='a';
-  // });
+  for(var i=0; i<buttonArray.length; i++)
+  {
+    buttonArray[i].innerText=answerArray[i];
+  }
 }
 
 
@@ -37,7 +67,7 @@ function alreadyExist(inputArray, target)
 {
   for(var i=0; i<inputArray; i++)
   {
-    if(inputArray[i]===target)
+    if(inputArray[i]==target)
       return true;
   }
   return false;
@@ -50,7 +80,7 @@ function getFourTranslation(correct,vocabSet)
   while(finalarray.length!=4)
   {
     var random=Math.floor(Math.random() * vocabSet.length);
-    var newdef=vocabSet[random].translations[0];
+    var newdef=vocabSet[random].translations;
     if(!alreadyExist(finalarray,newdef))
       finalarray.push(newdef);
   }
