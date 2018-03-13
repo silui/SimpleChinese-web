ae9d7da721846a737c15526f9cc090435e30bd67
silui <silui@ucsc.edu>
2018-03-13 09:15:29 -0700

    server side database access

--- router.js
+++ router.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var path = require('path');
+var admin = require("firebase-admin");
 var app = express();
 // var path = require('./watertester-32c5a-export.json');
+var serviceAccount = require("./firebase/watertester-32c5a-firebase-adminsdk-35nrs-8157cd13de.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://watertester-32c5a.firebaseio.com"
+});
 
 
 app.set('port',80);
 
 app.use(express.static(__dirname));
+
+app.get('/test',function(req,res){
+	console.log('get to testpage');
+	var dbRef=admin.database().ref("vocab");
+	dbRef.on("value",function(snapshot)
+	{
+		var vocabListAll = snapshot.val();
+		res.status(200).send(vocabListAll);
+	});
+
+	// res.status(200)
+					// .sendFile('homepage2.html', {root: path.join(__dirname,'./homepage')});
+			 // .send(dbRef);
+});
 
 app.get('/', function(req,res){
 	console.log('get to homepage');
@@ -14,8 +35,6 @@ app.get('/', function(req,res){
 		.status(200)
 		.sendFile('homepage2.html', {root: path.join(__dirname,'./homepage')});
 });
-
-
 
 app.get('/profile', function(req,res){
 	res
@@ -55,12 +74,7 @@ app.get('/quiz/level*-*',function(req,res){
 		 .sendFile('quiz.html',{root: path.join(__dirname,'./quizFolder')});
 });
 
-app.get('/test',function(req,res){
-	console.log('get to testpage')
-	res.status(200)
-					.sendFile('homepage2.html', {root: path.join(__dirname,'./homepage')});
-			 // .send('test');
-});
+
 
 var server = app.listen(app.get('port'),function(){
 	var port = server.address().port;
