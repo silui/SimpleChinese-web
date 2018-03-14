9dcdfcaee71b9c1e0d5e3091a93a0261480e298c
silui <silui@ucsc.edu>
2018-03-14 15:40:19 -0700

    Need to implement forgot password, email verification, fix styling, and strokes bug and we GUD

--- quizFolder/quiz.js
+++ quizFolder/quiz.js
@@ -3,6 +3,15 @@ var splitUrl = myMainSite.split(/level|-/g);
 var targetLevel=splitUrl[1]-1;
 var targetSetStart=(splitUrl[2]-1)*25;
 var dbRef=firebase.database().ref("vocab");
+document.getElementById("main").innerHTML=`
+<h2 class="head">
+<span>Level(${splitUrl[1]})</span>
+<span>Set(${splitUrl[2]})</span>
+<span>Quiz</span>
+</h2>
+<div id="quizArea"></div>
+<a class="button" onclick="submitPressed()">Submit</a>
+`;
 dbRef.on("value",function(snapshot)
 {
   var vocabListAll = snapshot.val();
@@ -10,31 +19,49 @@ dbRef.on("value",function(snapshot)
   var levelArray = curStudy[targetLevel];
   tempArray = Object.values(levelArray)
   globalWholeArray= tempArray.slice(targetSetStart,targetSetStart+25);
-  var buttonArr = document.getElementsByClassName('answer');
-  for(var i=0; i<buttonArr.length; i++)
-  {
-    buttonArr[i].addEventListener("click",function()
-    {
-      if(this.getAttribute("answerid")==correctbox)
-        correctness++;
-      // console.log(this.parentNode.children[0].innerText);
-      resultArray[2].push(this.parentNode.children[0].innerText);
-      next();
-    });
-  }
-  next();
+  setquizArea();
+  // var buttonArr = document.getElementsByClassName('answer');
+  // for(var i=0; i<buttonArr.length; i++)
+  // {
+  //   buttonArr[i].addEventListener("click",function()
+  //   {
+  //     if(this.getAttribute("answerid")==correctbox)
+  //       correctness++;
+  //     // console.log(this.parentNode.children[0].innerText);
+  //     resultArray[2].push(this.parentNode.children[0].innerText);
+  //     next();
+  //   });
+  // }
+  // next();
 });
-var resultArray = [];
+var resultArray = [];   //3xn matrix to store final result
 resultArray.push([]);
 resultArray.push([]);
 resultArray.push([]);
 var correctness = 0;
 var tempArray = [];
 var globalWholeArray=[];
-var iter=-1;
+var iter=0;
 var correctbox;
-function next()
+function submitPressed()
 {
+  //check if the correct box is selected
+  var rInputArr=document.getElementsByClassName("radioInput");
+  for(var i=0;i<4;i++)
+  {
+    if(rInputArr[i].checked==true)
+    {
+      if(i==correctbox)
+        correctness++;
+      // console.log(rInputArr[i].nextSibling.innerHTML)
+      resultArray[2].push(rInputArr[i].nextSibling.innerHTML);
+    }
+  }
+
+
+
+
+
   if(iter===globalWholeArray.length-1){
     var tableHTML=`<table border="1"><tr><th>character</th><th>correct answer</th><th>your pick</th></tr>`;
     for(var i=0; i<resultArray[0].length;i++)
@@ -42,33 +69,39 @@ function next()
       tableHTML+=`<tr><td>${resultArray[0][i]}</td><td>${resultArray[1][i]}</td><td>${resultArray[2][i]}</td></tr>`;
     }
     tableHTML+=`<table>${correctness}/25 is correct <button onclick="window.location.href='./'">Go Back</button>`;
-    document.getElementsByClassName("main")[0].innerHTML=tableHTML;
+    document.getElementById("main").innerHTML=tableHTML;
     updateScore(firebase.auth().currentUser.uid,splitUrl[1],splitUrl[2],correctness);
   }
   else {
     iter++;
-    setChineseChar(globalWholeArray[iter].hanzi);
-    var translationArray = getFourTranslation(globalWholeArray[iter].translations,globalWholeArray[iter].id,tempArray);
-    assignAnswer(document.getElementsByClassName('textField'),translationArray)
+    setquizArea();
   }
 }
 
-function setChineseChar(inputChar)
+function setquizArea()
 {
-  document.getElementById('charField').innerHTML=inputChar;
-  resultArray[0].push(inputChar);
-}
-function assignAnswer(textField, answerArray)
-{
-  for(var i=0; i<textField.length; i++)
+  resultArray[0].push(globalWholeArray[iter].hanzi);
+  var returnHTML=`<span id="vocabField">${globalWholeArray[iter].hanzi}</span>`;
+  returnHTML+='<div class="options">'
+  var fourAnswer=getFourTranslation(globalWholeArray[iter].translations,globalWholeArray[iter].id,tempArray);
+  for(var i=0;i<4;i++)
   {
-    textField[i].innerHTML='';
-    for(var k=0; k<answerArray[i].length;k++)
-    {
-      textField[i].innerHTML+=answerArray[i][k]+"<br>";
-    }
+    returnHTML+=`<div class="option"><input type="radio" name="Q1" class="radioInput"/><span>${fourAnswer[i].join(', ')}.</span></div>`
   }
+  returnHTML+='</div>'
+  document.getElementById("quizArea").innerHTML=returnHTML;
 }
+// function assignAnswer(textField, answerArray)
+// {
+//   for(var i=0; i<textField.length; i++)
+//   {
+//     textField[i].innerHTML='';
+//     for(var k=0; k<answerArray[i].length;k++)
+//     {
+//       textField[i].innerHTML+=answerArray[i][k]+"<br>";
+//     }
+//   }
+// }
 
 
 function shuffle(a) {
@@ -80,7 +113,7 @@ function shuffle(a) {
     for(var k=0; k<a.length; k++)
     {
       if(a[k]===correctOne)
-        correctbox=k+1;
+        correctbox=k;
     }
     return a;
 }
@@ -90,7 +123,7 @@ function getFourTranslation(correct,correctId,vocabSet)
 {
   var finalarray=[];
   finalarray.push(correct);
-  resultArray[1].push(correct);
+  resultArray[1].push(correct.join(', '));
   while(finalarray.length!=4)
   {
     var random=Math.floor(Math.random() * vocabSet.length);
